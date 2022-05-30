3e05255aa248099909b61588b0e4f4d50ffb76bc
EthanPunzalan <71811458+EthanPunzalan@users.noreply.github.com>
2022-05-31 00:14:14 +0400

    Project Upload

---
+++ html/signup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="landingPage.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="navBar">
+        <img src="/imgs/logo.png" alt="">
+        <a class="active" href="/html/landingPage.html">Home</a>
+        <a href="/html/pricingPage.html">Pricing</a>
+        <a href="#contact">Menu</a>
+        <a href="#login" class="loginBtn">Sign in</a>
+    </div>
+</body>
+</html>
